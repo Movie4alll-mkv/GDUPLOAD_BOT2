f328cd93dcbc72e9a466718fcefb2df3e77102af
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2022-11-28 18:13:06 +0500

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -15,7 +15,7 @@ try:
   if ENV:
     BOT_TOKEN = os.environ.get('BOT_TOKEN','5541380668:AAGH9yGr_4leFAgG35raDvAi1Q82ynMJGG0')
     APP_ID = os.environ.get('APP_ID','6534707')
-    API_HASH = os.environ.get('API_HASH'.'4bcc61d959a9f403b2f20149cbbe627a')
+    API_HASH = os.environ.get('API_HASH','4bcc61d959a9f403b2f20149cbbe627a')
     DATABASE_URL = os.environ.get('DATABASE_URL','mongodb+srv://Uploader:Uploader@cluster0.ba0ppxa.mongodb.net/?retryWrites=true&w=majority')
     SUDO_USERS = os.environ.get('SUDO_USERS','1430593323')
     SUPPORT_CHAT_LINK = os.environ.get('SUPPORT_CHAT_LINK','https://t.me/animecolony')
